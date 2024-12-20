3df2c04b054b646584da2fdc1bed7c8a312a6a54
krazor <jakeaquilina94@gmail.com>
2024-12-20 14:14:15 +1100

    update

--- include.sh
+++ include.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-CURL_LIBS="$(pkg-config --libs libcurl)"
-CURL_CFLAGS="$(pkg-config --cflags libcurl)"
-
-AC_ADD_LINK_FLAGS="$AC_ADD_LINK_FLAGS $CURL_LIBS"
-AC_ADD_COMPILE_FLAGS="$AC_ADD_COMPILE_FLAGS $CURL_CFLAGS" 
+# Add CURL linking flags directly
+AC_ADD_LINK_FLAGS="$AC_ADD_LINK_FLAGS -lcurl"
